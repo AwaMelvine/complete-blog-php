291a56459be9df0399609058de84d3acce6dbf74
Awa <melvineawa@gmail.com>
2017-12-29 09:27:03 +0100

    Project directory structure set up. Navbar styling done

---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>LifeBlog | Home</title>
+
+<!-- Google Fonts -->
+<link href="https://fonts.googleapis.com/css?family=Noto+Serif|Tangerine" rel="stylesheet">
+
+<!-- Styling for public area -->
+<link rel="stylesheet" href="static/css/public_styling.css">
+
+</head>
+<body>
+
+<!-- Navbar -->
+<div class="navbar">
+	<div class="logo_div">
+		<h1>LifeBlog</h1>
+	</div>
+	<ul>
+	  <li><a class="active" href="#home">Home</a></li>
+	  <li><a href="#news">News</a></li>
+	  <li><a href="#contact">Contact</a></li>
+	  <li><a href="#about">About</a></li>
+	</ul>
+</div>
+<!-- // Navbar -->
+
+
+
+
+</body>
+</html>
