4fbb8dcfc4bfdba14773289cc02ca80ea1f786a4
Awa <melvineawa@gmail.com>
2017-12-29 12:34:54 +0100

    Finished creating and styling banner on homepage

--- index.php
+++ index.php
@@ -4,7 +4,8 @@
 <title>LifeBlog | Home</title>
 
 <!-- Google Fonts -->
-<link href="https://fonts.googleapis.com/css?family=Noto+Serif|Tangerine" rel="stylesheet">
+<!-- <link href="https://fonts.googleapis.com/css?family=Noto+Serif|Tangerine" rel="stylesheet"> -->
+<link href="https://fonts.googleapis.com/css?family=Averia+Serif+Libre|Noto+Serif|Tangerine" rel="stylesheet">
 
 <!-- Styling for public area -->
 <link rel="stylesheet" href="static/css/public_styling.css">
@@ -26,7 +27,29 @@
 </div>
 <!-- // Navbar -->
 
+<!-- Banner -->
+<div class="banner">
+	<div class="welcome_msg">
+		<h1>Today's Inspiration</h1>
+		<p> 
+		    One day your life <br> 
+		    will flash before your eyes. <br> 
+		    Make sure it's worth watching. <br>
+			<span>~ Gerard Way</span>
+		</p>
+		<a href="#" class="btn">Join us!</a>
+	</div>
 
+	<div class="login_div">
+		<form action="" >
+			<h2>Login</h2>
+			<input type="text" name="username" placeholder="Username">
+			<input type="password" name="password"  placeholder="Password"> 
+			<button class="btn" type="submit">Sign in</button>
+		</form>
+	</div>
+</div>
+<!-- // Banner -->
 
 
 </body>
