c956648919f7597c111fee534fdf494014d4eef0
Awa <melvineawa@gmail.com>
2017-12-30 21:19:04 +0100

    Finished styling post listing on index page

--- index.php
+++ index.php
@@ -4,7 +4,6 @@
 <title>LifeBlog | Home</title>
 
 <!-- Google Fonts -->
-<!-- <link href="https://fonts.googleapis.com/css?family=Noto+Serif|Tangerine" rel="stylesheet"> -->
 <link href="https://fonts.googleapis.com/css?family=Averia+Serif+Libre|Noto+Serif|Tangerine" rel="stylesheet">
 
 <!-- Styling for public area -->
@@ -12,6 +11,8 @@
 
 </head>
 <body>
+
+<div class="container">
 
 <!-- Navbar -->
 <div class="navbar">
@@ -50,6 +51,57 @@
 	</div>
 </div>
 <!-- // Banner -->
+
+
+<!-- content -->
+	<div class="content">
+		<h2 class="content-title">Recent Articles</h2>
+		<hr>
+
+		<div class="post" style="margin-left: 0px;">
+			<img src="static/images/post_image1.jpg" class="post_image" alt="">
+			<a href="#" class="btn category">Inspiration</a>
+			<a href="#">
+				<div class="post_info">
+					<h3>One day your life will flash before your eyes. Make sure it's worth watching</h3>
+					<div class="info">
+						<span>Dec 25, 2017</span>
+						<span class="read_more">Read more...</span>
+					</div>
+				</div>
+			</a>
+		</div>
+		<div class="post" >
+			<img src="static/images/post_image2.jpg" class="post_image" alt="">
+			<a href="#" class="btn category">Self-Help</a>
+			<a href="#">
+				<div class="post_info">
+					<h3>One day your life will flash before your eyes. Make sure it's worth watching</h3>
+					<div class="info">
+						<span>Dec 25, 2017</span>
+						<span class="read_more">Read more...</span>
+					</div>
+				</div>
+			</a>
+		</div>
+		<div class="post" style="margin-right: 0px;">
+			<img src="static/images/post_image3.jpg" class="post_image" alt="">
+			<a href="#" class="btn category">Reading</a>
+			<a href="#">
+				<div class="post_info">
+					<h3>One day your life will flash before your eyes. Make sure it's worth watching</h3>
+					<div class="info">
+						<span>Dec 25, 2017</span>
+						<span class="read_more">Read more...</span>
+					</div>
+				</div>
+			</a>
+		</div>
+	</div>
+<!-- // content -->
+
+</div>
+<!-- // container -->
 
 
 </body>
